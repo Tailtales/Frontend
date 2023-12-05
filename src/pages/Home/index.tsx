721345a6875c85c21ca9b647b8512427597b3f07
ishagoyal <iamishagoyal@gmail.com>
2023-12-05 10:11:21 +0530

    Get puppies button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,34 @@
-import styled from 'styled-components'
-import backgroundImage from '../../assets/backgroundImage.png'
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import { useCallback } from "react";
 
 const Wrapper = styled.div`
   display: flex;
-`
-export default function Home(){
-    return(
-        <Wrapper>
-           
-        </Wrapper>
-    )
+  margin: auto;
+  padding-top: 164px;
+`;
+
+const PuppiesButton = styled.button`
+    border-radius: 50px;
+    background: #E19143;
+    border: none;
+    padding: 12px 54px;
+    color: #FFF;
+    font-family: sans-serif;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: normal;
+`;
+export default function Home() {
+  const history = useHistory();
+
+  const handleGetPuppies = useCallback(() => {
+    history.push("/mint");
+  }, [history]);
+  return (
+    <Wrapper>
+      <PuppiesButton onClick={() => handleGetPuppies()}>Get Puppies</PuppiesButton>
+    </Wrapper>
+  );
 }
